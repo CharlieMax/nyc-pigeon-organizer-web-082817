41cd97fda9adb33636ddad1d6657c785707d67bd
Charlie Max <dherzl@protonmail.com>
2017-08-09 18:13:07 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,42 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data[:gender].each do |k, v|
+    v.each do |name|
+      pigeon_list[name] =  {}
+    end
+  end
+  data.each do |key, value|
+    value.each do |k, v|
+      pigeon_list.each do |name, hash|
+        if v.include?(name)
+          pigeon_list[name][key] ||= []
+          pigeon_list[name][key] << k.to_s
+        end
+      end
+    end
+  end
+  pigeon_list
 end
+
+
+
+d ={
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+
+puts nyc_pigeon_organizer(d)
